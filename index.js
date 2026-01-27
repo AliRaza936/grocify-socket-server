c66c9cdff3a72b436c80d78239a5d95fb7fac810
Ali Raza <aliraza93644@gmail.com>
2026-01-27 12:24:35 +0500

    small issue solve

--- index.js
+++ index.js
@@ -11,18 +11,32 @@ app.use(express.json())
 const server = http.createServer(app)
 const port = process.env.PORT 
 
-const io = new Server(server,{
-    cors:{
-        origin:process.env.NEXT_BASE_URL
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: process.env.NEXT_BASE_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 io.on('connection',(socket)=>{
     console.log('user connected',socket.id)
 
-    socket.on('identity',async(userId)=>{
-        await axios.post(`${process.env.NEXT_BASE_URL}/api/socket/connect`,{userId,socketId:socket.id})
-    })
+  socket.on('identity', async (userId) => {
+  try {
+    await axios.post(
+      `${process.env.NEXT_BASE_URL}/api/socket/connect`,
+      {
+        userId,
+        socketId: socket.id,
+      }
+    );
+
+    console.log("Socket ID updated:", socket.id);
+  } catch (err) {
+    console.error("Identity error", err);
+  }
+});
     socket.on('updateLocation',async({userId,latitude,longitude})=>{
          console.log(userId,longitude,latitude)
          const location ={
@@ -44,15 +58,24 @@ io.on('connection',(socket)=>{
         await axios.post(`${process.env.NEXT_BASE_URL}/api/chat/save`,message)
         io.to(message.roomId).emit('send-message',message)
     })
-    socket.on("disconnect",()=>{
-        console.log('user disconnected',socket.id)
-    })
+   socket.on("disconnect", async () => {
+  console.log("user disconnected", socket.id);
+
+  try {
+    await axios.post(`${process.env.NEXT_BASE_URL}/api/socket/disconnect`, {
+      socketId: socket.id,
+    });
+  } catch (err) {
+    console.error("disconnect cleanup failed");
+  }
+});
 
 })
 app.post('/notify',(req,res)=>{
     const {event,data,socketId} = req.body
     if(socketId){
         io.to(socketId).emit(event,data)
+        console.log('socketIdssss',socketId)
     }else{
         io.emit(event,data)
     }
